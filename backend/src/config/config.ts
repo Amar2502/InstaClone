b66a1eafdaf33f85bc7ab7e784773baf3fbefdee
Amar2502 <amarpandey2502@gmail.com>
2025-06-14 11:58:20 +0530

    switched from prisma to ray mysql for learning

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,7 +4,9 @@ dotenv.config()
 
 const config = {
     PORT: process.env.PORT || 5000,
-    DATABASE_URL: process.env.DATABASE_URL|| "",
+    DB_HOST: process.env.DB_HOST || "",
+    DB_USER: process.env.DB_USER || "root",
+    DB_PASSWORD: process.env.DB_PASSWORD || "",
 }
 
 export default config;
--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,17 +1,23 @@
-import { PrismaClient } from "../generated/prisma";
+import mysql from "mysql2/promise";
+import config from "./config";
 
-// Create the Prisma Client instance
-const prisma = new PrismaClient(); 
+export const pool = mysql.createPool({
+  host: config.DB_HOST,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: "instaclone",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-// Optional: Connect immediately to log status
 export async function connectDB() {
-  try {
-    await prisma.$connect();
-    console.log("✅ Connected to MySQL database using Prisma");
-  } catch (error) {
-    console.error("❌ Failed to connect to the database:", error);
-    process.exit(1);
+    try {
+      const connection = await pool.getConnection();
+      console.log("✅ MySQL Database connected successfully.");
+      connection.release(); // Release the connection back to the pool
+    } catch (err) {
+      console.error("❌ Database connection failed:", err);
+      process.exit(1); // Exit the app if DB fails
+    }
   }
-}
-
-export default prisma;

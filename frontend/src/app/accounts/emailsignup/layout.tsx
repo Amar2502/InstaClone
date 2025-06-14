708cf3519611791647fc7d22241c79787dbcf0ff
Amar2502 <amarpandey2502@gmail.com>
2025-06-14 11:20:23 +0530

    made login and signup pages

---
+++ frontend/src/app/accounts/emailsignup/layout.tsx
@@ -0,0 +1,9 @@
+// app/accounrs/emailsignup/layout.tsx
+export const metadata = {
+  title: "Sign up - Instagram",
+  description: "Private dashboard area",
+};
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return <section>{children}</section>;
+}
